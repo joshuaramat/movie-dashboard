c3c5af4d0482a6cd61e3e49d4c1fd32163fe4541
joshuaramat <87837577+joshuaramat@users.noreply.github.com>
2022-01-08 21:28:23 -0800

    added registration routes and controllers with approval and rejection controllers

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,14 +4,24 @@ const multer = require('multer');
 
 const UserController = require('../controllers/UserController');
 const EventController = require('../controllers/EventController');
+const RegistrationController = require('../controllers/RegistrationController');
+const ApprovalController = require('../controllers/ApprovalController');
+const RejectionController = require('../controllers/RejectionController');
 
 const uploadConfig = require('../config/upload');
+const RegistrationController = require('../controllers/RegistrationController');
 const upload = multer(uploadConfig);
 
 
 router.get('/status', (req, res) => {
     res.send({ status: 200 })
 });
+
+// Registration Routes
+router.post('/registration/:eventId', RegistrationController.create);
+router.get('/registration/:registration_id', RegistrationController.getRegistration);
+router.post('/registration/:registration_id/approval', ApprovalController.approval);
+router.post('/registration/:registration_id/rejection', RejectionController.rejection);
 
 // Event Routes
 router.post('/event', upload.single('thumbnail'), EventController.createEvent);
