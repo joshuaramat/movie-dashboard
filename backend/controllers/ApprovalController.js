d3b7cdc1c28d28f0e9befee4f0a8fad08cd99844
SharksWithLazerbeams <Broccollikilla@gmail.com>
2022-01-11 08:23:30 -0800

    updates

--- backend/controllers/ApprovalController.js
+++ backend/controllers/ApprovalController.js
@@ -1,38 +1,19 @@
 const Registration = require('../models/Registration');
 
 module.exports = {
-    async create(req, res) {
-        const { user_id } = req.headers;
-        const { eventId } = req.params;
-        const { date } = req.body;
-
-        const registration = await Registration.create({
-            user: user_id,
-            event: eventId,
-            date
-        })
-
-        await registration
-            .populate('event')
-            .populate('user', '-password')
-            .execPopulate();
-
-        return res.json(registration)
-    },
-
-    async getRegistration(req, res) {
+    async approval(req, res) {
         const { registration_id } = req.params;
         try {
-            const registration = await Registration.findById(registration_id)
-            await registration
-                .populate('event')
-                .populate('user', '-password')
-                .execPopulate();
+            const registration = await Registration.findById(registration_id);
 
-            return res.json(registration)
+            registration.approved = true;
+
+            await registration.save();
+
+            return res.json(registration);
+
         } catch (error) {
-            return res.status(400).json({ message: "Registration not found" })
+            return res.status(400).json(error)
         }
-
     }
 }
