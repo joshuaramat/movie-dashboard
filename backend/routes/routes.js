26e7f47dd50583f69a16edb385c9338d7d1d280b
joshuaramat <87837577+joshuaramat@users.noreply.github.com>
2022-01-12 20:15:33 -0800

    started working on frontend

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,40 +3,32 @@ const multer = require('multer')
 
 const UserController = require('controllers/UserController')
 const EventController = require(".controllers/EventController");
-const DashboardController = require('./controllers/DashboardController');
-const LoginController = require('./controllers/LoginController');
-const RegistrationController = require('../controllers/RegistrationController');
-const ApprovalController = require('../controllers/ApprovalController');
-const RejectionController = require('../controllers/RejectionController');
-const uploadConfig = require('./config/upload');
+const DashboardController = require('./controllers/DashboardController')
+const LoginController = require('./controllers/LoginController')
+const uploadConfig = require('./config/upload')
 
-const router = express.Router();
+const routes = express.Router();
 const upload = multer(uploadConfig);
 
-router.get('/status', (req, res)=> {
+routes.get('/status', (req, res)=> {
     res.send({ status: 200 })
 })
 
-// Registration
-router.post('/registration/:eventId', RegistrationController.create);
-router.get('/registration/:registration_id', RegistrationController.getRegistration);
-router.post('/registration/:registration_id/approvals', ApprovalController.approval);
-router.post('/registration/:registration_id', RejectionController.rejection);
 
 //Login
-router.post('/login', LoginController.store)
+routes.post('/login', LoginController.store)
 
 // Dashboard
-router.get('/dashboard/:movie', DashBoardController.getAllEvents)
-router.get('/dashboard', DashboardController.getAllEvents)
-router.get('/event/:eventId', DashboardController.getEventById)
+routes.get('/dashboard/:movie', DashBoardController.getAllEvents)
+routes.get('/dashboard', DashboardController.getAllEvents)
+routes.get('/event/:eventId', DashboardController.getEventById)
 
 //Events
-router.post('/event', upload.single("thumbnail"),EventController.createEvent)
-router.delete('/event/:eventId', EventController.delete)
+routes.post('/event', upload.single("thumbnail"),EventController.createEvent)
+routes.delete('/event/:eventId', EventController.delete)
 
 //User
-router.post('/user/register', UserController.createUser)
-router.get('/user/:userId', UserController.getUserById)
+routes.post('/user/register', UserController.createUser)
+routes.get('/user/:userId', UserController.getUserById)
 
-module.exports = router;
+module.exports = routes;
